860fbab58507f5634b15f4a6945b11acaa7d074c
shyang12 <85710913+shyang12@users.noreply.github.com>
2023-01-12 18:00:04 +0900

    2023.01.12 / 레이아웃 및 게시물 어댑터 생성

---
+++ app/src/main/java/com/dbsh/skumarket/model/SellingModelData.kt
@@ -0,0 +1,12 @@
+package com.dbsh.skumarket.model
+
+data class SellingModelData (
+    val sellerId : String,
+    val title : String,
+    val posttime: Long,
+    val price : String,
+    val imageUrl : String
+) {
+    // 파이어베이스에 클래스 단위로 올리려면 인자빈생성자가 필요함
+    constructor() : this ("", "", 0, "", "")
+}
---
+++ app/src/main/java/com/dbsh/skumarket/view/SellingAdapter.kt
@@ -0,0 +1,76 @@
+package com.dbsh.skumarket.view
+
+
+
+import java.text.SimpleDateFormat
+import java.util.*
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.dbsh.skumarket.databinding.SellingItemBinding
+
+import com.dbsh.skumarket.model.SellingModelData
+
+class SellingAdapter(val onItemClicked: (SellingModelData) -> Unit) : ListAdapter<SellingModelData, SellingAdapter.ViewHolder>(diffUtil) {
+    inner class ViewHolder(private val binding: SellingItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        @SuppressLint("SimpleDateFormat")
+        fun bind(sellingModelData: SellingModelData) {
+
+            // Long 형식에서 날짜로 바꾸기.
+            val format = SimpleDateFormat("MM월 dd일")
+            val date = Date(sellingModelData.posttime) // Long -> Date
+
+            binding.titleText.text = sellingModelData.title
+            binding.dateText.text = format.format(date).toString()
+            binding.priceText.text = sellingModelData.price
+
+            // glide로 이미지 불러오기;
+            if (sellingModelData.imageUrl.isNotEmpty()) {
+                Glide.with(binding.thumbnailImg)
+                    .load(sellingModelData.imageUrl)
+                    .into(binding.thumbnailImg)
+            }
+
+            binding.root.setOnClickListener {
+                onItemClicked(sellingModelData)
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            SellingItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<SellingModelData>() {
+            override fun areItemsTheSame(oldItem: SellingModelData, newItem: SellingModelData): Boolean {
+                // 현재 노출하고 있는 아이템과 새로운 아이템이 같은지 비교;
+                return oldItem.posttime == newItem.posttime
+            }
+
+            override fun areContentsTheSame(oldItem: SellingModelData, newItem: SellingModelData): Boolean {
+                // equals 비교;
+                return oldItem == newItem
+
+            }
+
+        }
+    }
+}
